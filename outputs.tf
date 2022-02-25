4cf0da59a968cbb78465403b65a327a998e039c9
Pongsak Sanguanwong <pongsak.sanguanwong@gmail.com>
2022-02-25 10:51:56 +0700

    add output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "redis_host" {
   description = "Hostname of the redis host in the replication group."
   value       = aws_elasticache_replication_group.elasticache.primary_endpoint_address
 }
+
+output "security_group_id "{
+  description = "Security group id for the elasticache."
+  value       = aws_security_group.elasticache.id
+}
